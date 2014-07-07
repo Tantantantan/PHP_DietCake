33809d30ea211ffc38b7beffcee46a0a741209a8
Jonathan Magay <jonathan@UNIT081.(none)>
2014-07-07 16:23:54 +0800

    1. fix the single quotes and brackets placements from 3.2 database settings and 3.8 View Modification 2. fix SimpleDBI.php by adding require_once __DIR__. '/SimpleDBIstatement.php'

--- board/app/config/bootstrap.php
+++ board/app/config/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+
+// vendor
+require_once VENDOR_DIR.'SimpleDBI/src/SimpleDBI.php';
+
 // application
 require_once APP_DIR.'app_controller.php';
 require_once APP_DIR.'app_model.php';
@@ -7,14 +11,12 @@ require_once APP_DIR.'app_exception.php';
 
 // helpers
 require_once HELPERS_DIR.'html_helper.php';
+require_once HELPERS_DIR.'validation_helper.php';
 
 // config
 require_once CONFIG_DIR.'log.php';
 require_once CONFIG_DIR.'router.php';
 require_once CONFIG_DIR.'database.php';
-
-// vendor
-require_once VENDOR_DIR.'SimpleDBI/SimpleDBI.php';
 
 spl_autoload_register(function($name) {
     $filename = Inflector::underscore($name) . '.php';
