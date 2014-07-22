510a53ac2a58bf7ccd721617b0618b34f68a809c
Tantantantan <j.magay@klab.com>
2014-07-22 16:30:28 +0800

    Update index.php
    change $v to $thread

--- board/app/views/thread/index.php
+++ board/app/views/thread/index.php
@@ -2,7 +2,7 @@
 
 <ul>
   <?php foreach ($threads as $thread): ?>
-  <li><a href="<?php eh(url('thread/view', array('thread_id' => $v->id))) ?>"><?php eh($v->title) ?></a></li>
+  <li><a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))) ?>"><?php eh($thread->title) ?></a></li>
   <?php endforeach ?>
 </ul>
 
