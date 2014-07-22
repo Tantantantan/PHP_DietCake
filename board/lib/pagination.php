2615a996087fb11560d884304f371bb88f89629c
Tantantantan <j.magay@klab.com>
2014-07-22 10:33:40 +0800

    Update pagination.php
    remove excessive comments and add docblock to describe methods

--- board/lib/pagination.php
+++ board/lib/pagination.php
@@ -1,19 +1,29 @@
 <?php
 class Pagination{
 
-    const MAX_ROWS = 3;         //number of rows displays
-    const PAGE_LIMITS = 3;      //number of link pages
+    const MAX_ROWS = 3;     
+    const PAGE_LIMITS = 3;
+    //current page initialization at first page
     public static $current_page = 1;
-    
-    public static function setEndPage($total_rows){
+    /**
+     * set last page of threads
+     * @param total rows
+     * @return int of page
+     */
+    public static function setEndPage($total_rows)
+    {
         if ($total_rows <= self::MAX_ROWS) {
             return 1;
         }
         return ceil($total_rows/self::MAX_ROWS);
-    }//end page
-
-    public static function buildPages($page_number,$total_rows){
-
+    }
+    /**
+     * Build Page Links
+     * @param $total_rows
+     * @return string
+     */
+    public static function buildPages($page_number,$total_rows)
+    {
         $last_page = self::setEndPage($total_rows);
 
         if (isset($page_number)) {
@@ -34,7 +44,7 @@ class Pagination{
             $page_links .= self::LeftLinks($i);
         }            
         
-        $page_links .= '' . self::$current_page . " "; //added spaces every links
+        $page_links .= '' . self::$current_page . " "; 
             
         for ($i = $next_page; $i <= $last_page; $i++) {
             $page_links .= self::RightLinks($i,$right_pagenum);
@@ -42,50 +52,62 @@ class Pagination{
 
         $page_links .= self::NextLinks(self::$current_page, $last_page, $next_page);
         return $page_links;
-    }//end of page_links
-
-    ##############################used methods in BuildPages()######################################
-
-        public static function PreviousLinks($page_number, $previous_page)
-        {
-            if ($page_number > 1){
+    }
+    /**
+     *Previous Links
+     * @param $page_number, $previous_page
+     * @return $previous_page
+     */
+    public static function PreviousLinks($page_number, $previous_page)
+    {
+        if ($page_number > 1){
             return '<a href="?page=' . $previous_page . '">Previous</a> ';
-            }
-            else{
+        }
+        else{
             return null;
-            }
-            
         }
-
-        public static function LeftLinks($page_number)
-        {  
-            if ($page_number > 0){
-                return '<a href="?page=' . $page_number . '">' . $page_number . '</a> '; 
-            }
-            else{
-                return null;
-            }
-        
+    }
+    /**
+     *create Left Links
+     * @param page_number
+     * @return $page_number
+     */
+    public static function LeftLinks($page_number)
+    {  
+        if ($page_number > 0){
+            return '<a href="?page=' . $page_number . '">' . $page_number . '</a> '; 
         }
-
-        public static function RightLinks($page_number, $right_pagenumber)
-        {
-            if ($page_number <= $right_pagenumber) {
-                return '<a href="?page=' . $page_number . '">' . $page_number . '</a> ' ;
-            }
-            else{
-                return null;
-            }
+        else{
+            return null;
         }
-
-        public static function NextLinks($page_number, $last_page, $next_page)
-        {
-            if ($page_number != $last_page){
-                return ' <a href="?page=' . $next_page . '">Next</a> ';
-            }
-            else{
-                 return null;       
-            }
+    }
+    /**
+     *create Right Links
+     * @param $page_number, $roght_pagenumber
+     * @return $page_number
+     */
+    public static function RightLinks($page_number, $right_pagenumber)
+    {
+        if ($page_number <= $right_pagenumber) {
+            return '<a href="?page=' . $page_number . '">' . $page_number . '</a> ' ;
         }
+        else{
+            return null;
+        }
+    }
+    /**
+     *create Next Links
+     * @param $page_number, $last_page, $next_page
+     * @return $next_page
+     */
+    public static function NextLinks($page_number, $last_page, $next_page)
+    {
+        if ($page_number != $last_page){
+            return ' <a href="?page=' . $next_page . '">Next</a> ';
+        }
+        else{
+             return null;       
+        }
+    }
 }
 ?>
