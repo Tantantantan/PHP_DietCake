49f4f858e991561b8ffc59eaf8ed6f3a62a60b77
Tantantantan <j.magay@klab.com>
2014-07-22 11:55:48 +0800

    Update index.php
    replace $v as $thread in L4

--- board/app/views/thread/index.php
+++ board/app/views/thread/index.php
@@ -1,7 +1,7 @@
 <h1>All threads</h1>
 
 <ul>
-  <?php foreach ($threads as $v): ?>
+  <?php foreach ($threads as $thread): ?>
   <li><a href="<?php eh(url('thread/view', array('thread_id' => $v->id))) ?>"><?php eh($v->title) ?></a></li>
   <?php endforeach ?>
 </ul>
