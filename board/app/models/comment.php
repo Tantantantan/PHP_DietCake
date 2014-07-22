f43631726438ad2d9d21367f9b2e0191483c6a5a
Tantantantan <j.magay@klab.com>
2014-07-22 14:53:42 +0800

    Update comment.php
    change check_length to validate length

--- board/app/models/comment.php
+++ board/app/models/comment.php
@@ -4,12 +4,12 @@ class Comment extends AppModel{
     public $validation = array(
         'username' => array(
             'length' => array(
-                'check_length', MIN_COM_USERNAME, MAX_COM_USERNAME,
+                'validate_length', MIN_COM_USERNAME, MAX_COM_USERNAME,
             ),
         ),
         'body' => array(
             'length' => array(
-                'check_length', MIN_COM_BODY, MAX_COM_BODY,
+                'validate_length', MIN_COM_BODY, MAX_COM_BODY,
             ),
         ),
     );//end of array
